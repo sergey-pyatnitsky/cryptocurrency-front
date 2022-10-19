2da87d40d1d346158c3d5c4b24d964bc2fdbdd11
Сергей <63928767+sergey-pyatnitsky@users.noreply.github.com>
2022-10-19 11:09:08 +0300

    Added ColorModeContext and ThemeWrapper *added icons for dark\light theme *deleted unnecessary files

---
+++ src/com/cryptocurrency/wrapper/ThemeWrapper.tsx
@@ -0,0 +1,66 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React, { ReactNode } from 'react'
+import ColorModeContext from '../context/ColorModeContext'
+import { amber, deepOrange } from '@mui/material/colors';
+import CssBaseline from '@mui/material/CssBaseline';
+
+interface Props {
+    children?: ReactNode
+}
+
+function ThemeWrapper({ children }: Props) {
+
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                    ...(mode === 'light'
+                        ? {
+                            // palette values for light mode
+                            primary: amber,
+                            divider: amber[200],
+                            text: {
+                                primary: '#1e2329',
+                                secondary: '#707a8a',
+                            },
+                        }
+                        : {
+                            // palette values for dark mode
+                            primary: deepOrange,
+                            divider: '#848e9c',
+                            background: {
+                                default: '#181a20',
+                                paper: '#181a20',
+                            },
+                            text: {
+                                primary: '#eaecef',
+                                secondary: '#848e9c',
+                            },
+                        }),
+                },
+            }),
+        [mode],
+    );
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    )
+}
+
+export default ThemeWrapper;
