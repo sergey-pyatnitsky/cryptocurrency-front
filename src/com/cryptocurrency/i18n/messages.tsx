bcf7282ddf239ee81fb0ca6b7d8f1157dbca8c57
Сергей <63928767+sergey-pyatnitsky@users.noreply.github.com>
2023-01-16 16:27:28 +0300

    Added error handling and i18n in the authorization form

--- src/com/cryptocurrency/i18n/messages.tsx
+++ src/com/cryptocurrency/i18n/messages.tsx
@@ -2,19 +2,31 @@ import { LOCALES } from './locales';
 
 export const messages = {
   [LOCALES.ENGLISH]: {
+    //Error
+      //Alert Title
+    error: 'Error',
+    success: "Success",
+    warning: "Warning",
+      //Input
+    error_incorrect_entry: 'Incorrect entry',
+
     //Btn
     continue_btn: 'Next',
     yes_btn: 'Yes',
     no_btn: 'No',
 
     //AuthReg Form
-    login_title: 'Binance Login',
+    login_title: 'Authorization',
     continue_with_btn: 'or continue with',
     continue_google_btn: 'Google',
     login_image_text: 'Buy Crypto in Minutes',
-    login_reg_link_text: 'Create a Binance Account',
+    login_reg_link_text: 'Create an account',
     reg_title: 'Create account',
     reg_auth_link_text: 'Already registered?',
+      //Auth Error
+    error_user_not_found: "User not found",
+    error_invalid_password: "Invalid password",
+    error_access_denied: "Access denied",
 
     //Logout
     logout_title: 'Do you really want to log out?',
@@ -31,19 +43,31 @@ export const messages = {
     coins_table_column_4: 'Market Cap',
   },
   [LOCALES.RUSSIAN]: {
+    //Error
+      //Alert Title
+    error: 'Ошибка',
+    success: "Успешно",
+    warning: "Предупреждение",
+      //Input
+    error_incorrect_entry: 'Неверный ввод',
+
     //Btn
     continue_btn: 'Далее',
     yes_btn: 'Да',
     no_btn: 'Нет',
 
     //AuthReg From
-    login_title: 'Binance Login',
+    login_title: 'Авторизация',
     continue_with_btn: 'или продолжить с',
     continue_google_btn: 'Google',
     login_image_text: 'Покупайте криптовалюту за считанные минуты',
-    login_reg_link_text: 'Создать аккаунт Binance',
+    login_reg_link_text: 'Создать аккаунт',
     reg_title: 'Создание аккаунта',
     reg_auth_link_text: 'Уже зарегистрированы?',
+      //Auth Error
+    error_user_not_found: "Пользователь не найден",
+    error_invalid_password: "Неверный пароль",
+    error_access_denied: "Доступ запрещен",
 
     //Logout
     logout_title: 'Вы действительно хотите выйти из системы?',
