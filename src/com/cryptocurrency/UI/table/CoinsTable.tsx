b53e9601b2ad5a84d1c62f2159f64ebcb35ffaef
Сергей <63928767+sergey-pyatnitsky@users.noreply.github.com>
2023-01-28 16:57:37 +0300

    The coin table is edited in MUI DataGrid with sorting, filtering, etc

--- src/com/cryptocurrency/UI/table/CoinsTable.tsx
+++ src/com/cryptocurrency/UI/table/CoinsTable.tsx
@@ -1,49 +1,14 @@
-import { useEffect, useState } from "react";
-import {
-  Container,
-  TableCell,
-  LinearProgress,
-  Typography,
-  TextField,
-  TableBody,
-  TableRow,
-  TableHead,
-  TableContainer,
-  Table,
-  Paper,
-} from "@mui/material";
-import Pagination from '@mui/material/Pagination';
-import axios from "axios";
-import { CryptoState } from "../../context/CryptoContext";
-import { CoinList } from "../../config/api";
-import { useIntl } from 'react-intl';
-import { useNavigate } from 'react-router-dom';
+import {useEffect, useState} from "react";
+import {Container, LinearProgress, TextField, Typography,} from "@mui/material";
+import {CryptoState} from "../../context/CryptoContext";
+import {useIntl} from 'react-intl';
+import {useNavigate} from 'react-router-dom';
+import CoinProps from "../../model/coin";
+import CoinService from "../../service/CoinService";
+import ResponseProps from "../../model/response";
+import ErrorProps from "../../model/error";
 
-interface ICoinProps {
-  "id": string,
-  "name": string,
-  "symbol": string,
-  "image": string,
-  "ruDescription": string,
-  "enDescription": string,
-  "coinMarket": {
-    "id": number,
-    "designation": {
-      "name": string,
-      "symbol": string
-    },
-    "currentPrice": number,
-    "marketCap": number,
-    "fullyDilutedValuation": number,
-    "high24h": number,
-    "low24h": number,
-    "priceChangePercentage24h": number,
-    "marketCapChangePercentage24h": number,
-    "circulatingSupply": number,
-    "totalSupply": number,
-    "lastUpdated": string
-  }[]
-}
+import {DataGrid, GridColDef, GridRowsProp, ruRU} from '@mui/x-data-grid';
 
 export function numberWithCommas(x: number) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -56,157 +21,158 @@ export default function CoinsTable() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
-  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState<number>(5);
 
-  const { currency, symbol } = CryptoState();
-
-  const fetchCoins = async () => {
-    setLoading(true);
-    // let config = { headers: { Authorization: sessionStorage.getItem("token"), } }
-    const { data } = await axios.get(CoinList(currency, search));
-
-    setCoins(data);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchCoins();
-  }, [currency]);
+  const {currency, symbol} = CryptoState();
 
   const handleSearch = () => {
     return coins.filter(
-      (coin: ICoinProps) =>
+      (coin: CoinProps) =>
         coin.name.toLowerCase().includes(search) ||
         coin.symbol.toLowerCase().includes(search)
     );
   };
 
+  const rows: GridRowsProp = handleSearch().map((coin: CoinProps) => {
+    return {
+      id: coin.id, col1: coin.name, col2: coin.coinMarket[0].currentPrice,
+      col3: coin.coinMarket[0].priceChangePercentage24h, col4: coin.coinMarket[0].marketCap, col5: coin
+    };
+  });
+
+  const columns: GridColDef[] = [
+    {
+      field: 'col1',
+      flex: 1,
+      minWidth: 100,
+      maxWidth: 500,
+      headerName: intl.formatMessage({id: 'coins_table_column_1'}),
+      renderCell: (params: any) => {
+        return (
+          <>
+            <img src={params.row.col5.image} alt={params.row.col5.name} height="50" style={{marginBottom: 10}}/>
+            <div style={{display: "flex", flexDirection: "column"}}>
+              <span style={{textTransform: "uppercase", fontSize: 22,}}>{params.row.col5.symbol}</span>
+              <span style={{color: "darkgrey"}}>{params.row.col5.name}</span>
+            </div>
+          </>
+        )
+      }
+    },
+    {
+      field: 'col2', flex: 1, minWidth: 50, maxWidth: 150, headerName: intl.formatMessage({id: 'coins_table_column_2'}),
+      renderCell: (params: any) => {
+        return (
+          <>
+            {symbol}{" "}
+            {numberWithCommas(Number(params.row.col5.coinMarket[0].currentPrice.toFixed(2)))}
+          </>
+        )
+      }
+    },
+    {
+      field: 'col3', flex: 1, minWidth: 50, maxWidth: 150, headerName: intl.formatMessage({id: 'coins_table_column_3'}),
+      renderCell: (params: any) => {
+        const profit: any = params.row.col5.coinMarket[0].priceChangePercentage24h > 0;
+        return (
+          <div
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"}
+            {params.row.col5.coinMarket[0].priceChangePercentage24h.toFixed(2)}%
+          </div>
+        )
+      }
+    },
+    {
+      field: 'col4',
+      flex: 1,
+      minWidth: 100,
+      maxWidth: 200,
+      headerName: intl.formatMessage({id: 'coins_table_column_4'}),
+      renderCell: (params: any) => {
+        return (
+          <>
+            {symbol}{" "}
+            {numberWithCommas(
+              Number(params.row.col5.coinMarket[0].marketCap.toString().slice(0, -6))
+            )}M
+          </>
+        )
+      }
+    },
+    {field: 'col5', flex: 0, maxWidth: 0, hide: true, hideable: false, pinnable: false},
+  ];
+
+  let locale
+  (localStorage.getItem('locale') == null || localStorage.getItem('locale') == 'ru-RU')
+    ? locale = ruRU.components.MuiDataGrid.defaultProps.localeText
+    : locale = undefined
+
+  useEffect(() => {
+    setLoading(true);
+    CoinService.fetchCoinList(currency, search)
+      .then((resp: ResponseProps) => {
+        setCoins(resp.data)
+        setLoading(false);
+      })
+      .catch((err: ErrorProps) => {
+        console.log(err)
+        setLoading(false)
+      });
+  }, [currency]);
+
   return (
-    <Container style={{ textAlign: "center" }}>
+    <Container style={{textAlign: "center", marginBottom: 40}}>
       <Typography
         variant="h4"
-        style={{ margin: 18, fontFamily: "Montserrat" }}
+        style={{margin: 18, fontFamily: "Montserrat"}}
       >
-        {intl.formatMessage({ id: 'coins_table_title' })}
+        {intl.formatMessage({id: 'coins_table_title'})}
       </Typography>
       <TextField
-        label={intl.formatMessage({ id: 'coins_table_search_text' })}
+        label={intl.formatMessage({id: 'coins_table_search_text'})}
         variant="outlined"
-        style={{ marginBottom: 20, width: "100%" }}
+        style={{marginBottom: 20, width: "100%"}}
         onChange={(e) => setSearch(e.target.value.toLowerCase())}
       />
-      <TableContainer component={Paper}>
-        {loading ? (
-          <LinearProgress style={{ backgroundColor: "gold" }} />
-        ) : (
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                {[
-                  intl.formatMessage({ id: 'coins_table_column_1' }),
-                  intl.formatMessage({ id: 'coins_table_column_2' }),
-                  intl.formatMessage({ id: 'coins_table_column_3' }),
-                  intl.formatMessage({ id: 'coins_table_column_4' })].map((head) => (
-                    <TableCell
-                      style={{
-                        fontWeight: "700",
-                        fontFamily: "Montserrat",
-                      }}
-                      key={head}
-                      align={head === intl.formatMessage({ id: 'coins_table_column_1' }) ? "left" : "right"}
-                    >
-                      {head}
-                    </TableCell>
-                  ))}
-              </TableRow>
-            </TableHead>
 
-            <TableBody>
-              {handleSearch()
-                .slice((page - 1) * 10, (page - 1) * 10 + 10)
-                .map((row: ICoinProps) => {
-                  const profit: any = row.coinMarket[0].priceChangePercentage24h > 0;
-                  return (
-                    <TableRow
-                      onClick={() => navigate(`/coins/${row.id}`)}
+      {loading
+        ? (
+          <LinearProgress style={{backgroundColor: "gold"}}/>
+        )
+        : (
+          <div>
+            <DataGrid autoHeight rows={rows} columns={columns} localeText={locale}
+                      pageSize={pageSize}
+                      onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                      rowsPerPageOptions={[5, 10, 20]}
+                      pagination
+                      rowHeight={100}
+                      onSelectionModelChange={row => navigate(`/coins/${row}`)}
                       sx={{
-                        cursor: "pointer",
-                        fontFamily: "Montserrat",
-                      }}
-                      key={row.name}
-                    >
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        style={{
-                          display: "flex",
-                          gap: 15,
-                        }}
-                      >
-                        <img
-                          src={row.image}
-                          alt={row.name}
-                          height="50"
-                          style={{ marginBottom: 10 }}
-                        />
-                        <div
-                          style={{ display: "flex", flexDirection: "column" }}
-                        >
-                          <span
-                            style={{
-                              textTransform: "uppercase",
-                              fontSize: 22,
-                            }}
-                          >
-                            {row.symbol}
-                          </span>
-                          <span style={{ color: "darkgrey" }}>
-                            {row.name}
-                          </span>
-                        </div>
-                      </TableCell>
-                      <TableCell align="right">
-                        {symbol}{" "}
-                        {numberWithCommas(Number(row.coinMarket[0].currentPrice.toFixed(2)))}
-                      </TableCell>
-                      <TableCell
-                        align="right"
-                        style={{
-                          color: profit > 0 ? "rgb(14, 203, 129)" : "red",
-                          fontWeight: 500,
-                        }}
-                      >
-                        {profit && "+"}
-                        {row.coinMarket[0].priceChangePercentage24h.toFixed(2)}%
-                      </TableCell>
-                      <TableCell align="right">
-                        {symbol}{" "}
-                        {numberWithCommas(
-                          Number(row.coinMarket[0].marketCap.toString().slice(0, -6))
-                        )}
-                        M
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        )}
-      </TableContainer>
-
-      <Pagination
-        count={Number((handleSearch()?.length / 10).toFixed(0))}
-        style={{
-          padding: 20,
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-        }}
-        onChange={(_, value) => {
-          setPage(value);
-          window.scroll(0, 450);
-        }}
-      />
+                        boxShadow: 2,
+                        border: 0,
+                        backgroundColor: 'background.paper',
+                        "& .MuiDataGrid-columnHeaders": {
+                          fontWeight: "bold",
+                          fontSize: 16,
+                          fontFamily: "Montserrat",
+                          color: "text.primary"
+                        },
+                        '& .MuiDataGrid-cell': {
+                          height: '100',
+                        },
+                        '& .MuiDataGrid-row': {
+                          height: '200',
+                        },
+                      }}/>
+          </div>
+        )
+      }
     </Container>
   );
 }
--- src/com/cryptocurrency/service/CoinService.ts
+++ src/com/cryptocurrency/service/CoinService.ts
@@ -4,7 +4,11 @@ import {API_BASE_URL} from "./CommonService";
 class CoinService {
 
   async fetchTrendingCoins(currency: string) {
-    return await axios.get(API_BASE_URL + `/public/coin/getTrendingCoins/${currency}`)
+    return await axios.get(API_BASE_URL + `/public/coin/getTrendingCoins/${currency.toLowerCase()}`)
+  }
+
+  async fetchCoinList(currency: string, search: string) {
+    return await axios.get(API_BASE_URL + `/public/coin/getCoinsList/${currency.toLowerCase()}?search=${search}`)
   }
 
 }
