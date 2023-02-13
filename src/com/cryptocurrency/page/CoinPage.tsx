84d68cb4ff8d8ea2f8a086eac90334691e94923a
Sergey Pyatnitsky <seruyfs@mail.ru>
2023-02-13 14:35:48 +0300

    Redesigned page *added i18n *added coin tracking processing

--- src/com/cryptocurrency/page/CoinPage.tsx
+++ src/com/cryptocurrency/page/CoinPage.tsx
@@ -1,192 +1,288 @@
-import { LinearProgress, Typography, Grid, Box, Button, Tooltip } from "@mui/material";
-import axios from "axios";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import StarBorderPurple500OutlinedIcon from "@mui/icons-material/StarBorderPurple500Outlined";
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { yellow } from "@mui/material/colors";
 import { useEffect, useState } from "react";
+import { useIntl } from "react-intl";
 import { useParams } from "react-router-dom";
-import { AddFavoriteCoin, SingleCoin } from "../config/api";
 import { CryptoState } from "../context/CryptoContext";
-import { numberWithCommas } from "../UI/table/CoinsTable";
-import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
-import StarBorderPurple500OutlinedIcon from '@mui/icons-material/StarBorderPurple500Outlined';
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
-import CoinMarketInfo from "../UI/coin/header/CoinMarketInfo";
-import CoinDescription from "../UI/coin/header/CoinDescription";
+import CoinProps from "../model/coin";
+import ErrorProps from "../model/error";
+import ResponseProps from "../model/response";
+import CoinService from "../service/CoinService";
+import CustomAlert from "../UI/alert/CustomAlert";
 import CoinTab from "../UI/coin/CoinTab";
 import EditCoinModal from "../UI/coin/EditCoinModal";
-
-type mapWithStringKeyAndNumberValue = {
-  [key: string]: number
-};
-
-interface ICoinProps {
-  id: string
-  name: string
-  symbol: string
-  description: {
-    en: string
-    ru: string
-  }
-  image: {
-    small: string
-  };
-  market_data: {
-    last_updated: string
-    current_price: mapWithStringKeyAndNumberValue
-    market_cap: mapWithStringKeyAndNumberValue
-    market_cap_change_percentage_24h: number
-    high_24h: mapWithStringKeyAndNumberValue
-    low_24h: mapWithStringKeyAndNumberValue
-    fully_diluted_valuation: mapWithStringKeyAndNumberValue
-    total_supply: number
-    max_supply: number
-  }
-  market_cap_rank: number
-}
+import CoinDescription from "../UI/coin/header/CoinDescription";
+import CoinMarketInfo from "../UI/coin/header/CoinMarketInfo";
+import { numberWithCommas } from "../UI/table/CoinsTable";
 
 interface ICoinPageProps {
-  role: string | null
+  role: string | null;
 }
 
 const CoinPage = ({ role }: ICoinPageProps) => {
+  const intl = useIntl();
   const { id } = useParams();
-  const [coin, setCoin] = useState<ICoinProps>();
-  const [favorite, setFavorite] = useState<boolean>();
+  const [loading, setLoading] = useState(false);
+  const [coin, setCoin] = useState<CoinProps>();
+
+  const [favorite, setFavorite] = useState<boolean>(false);
+  const [isInputPriceAlert, setInputPriceAlert] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleNumberChange = (e: any) => {
+    const onlyNums = e.target.value.replace(/[^0-9.]/g, "");
+    setInputValue(onlyNums);
+  };
+
+  const [priceAlert, setPriceAlert] = useState<boolean>(false);
+
+  const [error, setError] = useState({ alertError: false });
+  const [errorMsg, setErrorMsg] = useState("");
 
   const { currency, symbol } = CryptoState();
 
-  const fetchCoin = async () => {
-    const { data } = await axios.get(SingleCoin(id));
-    setCoin(data);
-  };
-
+  //fetch coin info
   useEffect(() => {
-    fetchCoin();
-  }, []);
+    setLoading(true);
+    CoinService.fetchSingleCoin(id, currency)
+      .then((resp: ResponseProps) => {
+        setCoin(resp.data);
+        setLoading(false);
+      })
+      .catch((err: ErrorProps) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, [currency]);
+
+  //fetch user coin info
+  useEffect(() => {
+    CoinService.fetchUserCoinInfo(id, sessionStorage.getItem("username"))
+      .then((resp: ResponseProps) => {
+        if (resp.status === 200) {
+          setPriceAlert(true);
+          setError({ alertError: false });
+          setFavorite(resp.data.favorite);
+          setPriceAlert(resp.data.existPriceAlert);
+        }
+      })
+      .catch(() => {
+        setFavorite(true);
+        setPriceAlert(true);
+      });
+  }, [coin]);
 
   //Modal handlers
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const handleFavorites = async (coin_id: string | undefined) => {
-    axios.defaults.headers.common['Authorization'] = sessionStorage.getItem("token")
+  // add favorite coin
+  const handleFavorites = (coin_id: string | undefined) => {
+    CoinService.addFavoriteCoin(coin_id, sessionStorage.getItem("username"))
+      .then((resp: ResponseProps) => {
+        if (resp.status === 200) setFavorite(true);
+      })
+      .catch((err: ErrorProps) => {
+        if (err.response.status === 401) {
+          setErrorMsg(
+            intl.formatMessage({ id: "access_denied_auth_to_continue" })
+          );
+        } else {
+          setErrorMsg(intl.formatMessage({ id: "error" }));
+        }
+        setError({ alertError: true });
+      });
+  };
 
-    await axios.post(AddFavoriteCoin(coin_id, sessionStorage.getItem("username")),)
-      .then(function (response) {
-        if (response.status == 200) setFavorite(true);
-      });;
-  }
+  // add price alert
+  const handlePriceAlert = () => {
+    setInputPriceAlert(!isInputPriceAlert);
+    CoinService.addPriceAlert(
+      id,
+      inputValue,
+      sessionStorage.getItem("username"),
+      currency
+    )
+      .then((resp: ResponseProps) => {
+        if (resp.status === 200) {
+          setPriceAlert(true);
+          setError({ alertError: false });
+        }
+      })
+      .catch((err: ErrorProps) => {
+        console.log(err);
+        if (err.response.status === 401) {
+          setErrorMsg(
+            intl.formatMessage({ id: "access_denied_auth_to_continue" })
+          );
+        } else {
+          setErrorMsg(intl.formatMessage({ id: "error" }));
+        }
+        setError({ alertError: true });
+      });
+  };
 
-  if (!coin) return <LinearProgress style={{ backgroundColor: "gold" }} />;
   return (
     <>
-      <div style={{ marginLeft: 100, marginTop: 50, marginRight: 100 }}>
-        <Grid container spacing={2} sx={{ width: 250 }}>
-          <Grid item xs={2}>
-            <img
-              src={coin?.image.small}
-              alt={coin?.name}
-              height="30"
-            />
-          </Grid>
-          <Grid item xs={10} >
-            <Typography variant="h6" >
-              <Box sx={{ fontWeight: 'bold' }}
-              >
-                {coin?.name + " (" + coin?.symbol.toUpperCase() + ")"}
-                {
-                  role != null && role == 'ADMIN'
-                    ? (
-                      <>
-                        <Button
-                          onClick={handleOpen}
-                          sx={{ minWidth: 0 }}
-                        >
-                          <ModeEditIcon sx={{ marginLeft: 0.1, height: 20 }} />
-                        </Button>
-                        <EditCoinModal open={open} handleClose={handleClose} coin={coin} />
-                      </>
-                    ) : (<></>)
-                }
+      {loading || !coin ? (
+        <LinearProgress style={{ backgroundColor: "gold" }} />
+      ) : (
+        <div style={{ marginLeft: 100, marginTop: 50, marginRight: 100 }}>
+          <Grid container spacing={2} sx={{ width: 250 }}>
+            <Grid item xs={2}>
+              <img src={coin.image} alt={coin.name} height="30" />
+            </Grid>
+            <Grid item xs={10}>
+              <Typography component={"div"} variant="h6">
+                <Box sx={{ fontWeight: "bold" }}>
+                  {coin.name + " (" + coin.symbol.toUpperCase() + ")"}
+                  {role != null && role === "ADMIN" ? (
+                    <>
+                      <Button onClick={handleOpen} sx={{ minWidth: 0 }}>
+                        <ModeEditIcon sx={{ marginLeft: 0.1, height: 20 }} />
+                      </Button>
+                      <EditCoinModal
+                        open={open}
+                        handleClose={handleClose}
+                        coin={coin}
+                      />
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </Box>
+              </Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <Box sx={{ fontWeight: "bold", fontSize: "h6.fontSize" }}>
+                {symbol}
+                {numberWithCommas(coin.coinMarket[0].currentPrice)}
+                {"  "}
               </Box>
-            </Typography>
-          </Grid>
-          <Grid item xs={5}>
-            <Box sx={{ fontWeight: 'bold', fontSize: 'h6.fontSize' }}>
-              {symbol}
-              {numberWithCommas(
-                coin?.market_data.current_price[currency.toLowerCase()]
-              )}
-              {"  "}
-            </Box>
-          </Grid>
-          <Grid item xs={7}>
-            <Box sx={{
-              fontWeight: 'bold',
-              fontSize: 'h6.fontSize',
-              color: coin?.market_data.market_cap_change_percentage_24h > 0 ? "rgb(14, 203, 129)" : "red"
-            }}>
-              {coin?.market_data.market_cap_change_percentage_24h > 0 ? '+' : ''}
-              {(Math.round(coin?.market_data.market_cap_change_percentage_24h * 10) / 10).toString() + "%"}
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={2} sx={{ width: 250, marginTop: 1 }}>
-          <Grid item xs={12}>
-            <Tooltip title="Add Price Alert">
-              <Button variant="outlined" sx={{ height: 35, marginRight: 1 }}
+            </Grid>
+            <Grid item xs={7}>
+              <Box
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "h6.fontSize",
+                  color:
+                    coin.coinMarket[0].marketCapChangePercentage24h > 0
+                      ? "rgb(14, 203, 129)"
+                      : "red",
+                }}
               >
-                <NotificationsActiveOutlinedIcon />
-              </Button>
-            </ Tooltip>
-            <Tooltip title="Добавить в отслеживаемые">
-              <Button
-                variant="outlined"
-                sx={{ height: 35, marginRight: 1 }}
-                onClick={(e) => {
-                  handleFavorites(id);
+                {coin.coinMarket[0].marketCapChangePercentage24h > 0 ? "+" : ""}
+                {(
+                  Math.round(
+                    coin.coinMarket[0].marketCapChangePercentage24h * 10
+                  ) / 10
+                ).toString() + "%"}
+              </Box>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2} sx={{ width: "40%", marginTop: 1 }}>
+            <Grid item xs={12}>
+              <Tooltip title={intl.formatMessage({ id: "add_price_alert" })}>
+                {!isInputPriceAlert ? (
+                  <Button
+                    variant="outlined"
+                    sx={{ height: 35, marginRight: 1 }}
+                    onClick={() => setInputPriceAlert(!isInputPriceAlert)}
+                    disabled={priceAlert}
+                  >
+                    <NotificationsActiveOutlinedIcon
+                      sx={{ color: priceAlert ? yellow[900] : null }}
+                    />
+                  </Button>
+                ) : (
+                  <>
+                    <Button
+                      variant="outlined"
+                      sx={{ height: 35, marginRight: 1 }}
+                      onClick={handlePriceAlert}
+                    >
+                      {intl.formatMessage({ id: "save" })}
+                    </Button>
+                    <TextField
+                      id="outlined-error-helper-text"
+                      value={inputValue}
+                      size="small"
+                      label={intl.formatMessage({ id: "coins_table_column_2" })}
+                      sx={{ width: "40%", marginRight: 1 }}
+                      onChange={handleNumberChange}
+                    />
+                  </>
+                )}
+              </Tooltip>
+              <Tooltip title={intl.formatMessage({ id: "add_to_favorite" })}>
+                <Button
+                  variant="outlined"
+                  sx={{ height: 35, marginRight: 1 }}
+                  onClick={() => handleFavorites(id)}
+                  disabled={favorite}
+                >
+                  <StarBorderPurple500OutlinedIcon
+                    sx={{ color: favorite ? yellow[900] : null }}
+                  />
+                </Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={1} sx={{ width: 400, marginTop: 2 }}>
+            <Grid item xs={12}>
+              <LinearProgress
+                sx={{ width: 400, height: 10, borderRadius: 10 }}
+                variant="determinate"
+                value={
+                  ((coin.coinMarket[0].currentPrice -
+                    coin.coinMarket[0].low24h) /
+                    (coin.coinMarket[0].high24h - coin.coinMarket[0].low24h)) *
+                  100
                 }
-                }
-              >
-                <StarBorderPurple500OutlinedIcon />
-              </Button>
-            </Tooltip>
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <Typography variant="subtitle2">
+                {numberWithCommas(coin.coinMarket[0].low24h)}
+              </Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <Typography variant="subtitle2">24H Range</Typography>
+            </Grid>
+            <Grid item xs={2} alignItems="left">
+              <Typography variant="subtitle2">
+                {numberWithCommas(coin.coinMarket[0].high24h)}
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
 
-        <Grid container spacing={1} sx={{ width: 400, marginTop: 2 }}>
-          <Grid item xs={12}>
-            <LinearProgress sx={{ width: 400, height: 10, borderRadius: 10 }} variant="determinate"
-              value={(coin?.market_data.current_price[currency.toLowerCase()] - coin?.market_data.low_24h[currency.toLowerCase()])
-                / (coin?.market_data.high_24h[currency.toLowerCase()] - coin?.market_data.low_24h[currency.toLowerCase()])
-                * 100} />
-          </Grid>
-          <Grid item xs={5}>
-            <Typography variant="subtitle2" >
-              {numberWithCommas(
-                coin?.market_data.low_24h[currency.toLowerCase()]
-              )}
-            </Typography>
-          </Grid>
-          <Grid item xs={5}>
-            <Typography variant="subtitle2" >
-              24H Range
-            </Typography>
-          </Grid>
-          <Grid item xs={2} alignItems="left">
-            <Typography variant="subtitle2" >
-              {numberWithCommas(
-                coin?.market_data.high_24h[currency.toLowerCase()]
-              )}
-            </Typography>
-          </Grid>
-        </Grid>
-
-
-        <CoinMarketInfo coin={coin} />
-        <CoinDescription coin={coin} />
-        <CoinTab coin={coin} />
-      </div>
+          <CoinMarketInfo coin={coin} />
+          <CoinDescription coin={coin} />
+          <CoinTab coin={coin} />
+        </div>
+      )}
+      {error.alertError ? (
+        <CustomAlert
+          severity={"error"}
+          errorMsg={errorMsg}
+          error={error}
+          setError={setError}
+        />
+      ) : null}
     </>
   );
 };
