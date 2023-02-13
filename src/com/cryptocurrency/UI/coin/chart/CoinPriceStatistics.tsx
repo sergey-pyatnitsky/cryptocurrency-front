0aaa224cdd6e6e4411ed2b7a3915f42e8cf1c267
Sergey Pyatnitsky <seruyfs@mail.ru>
2023-02-13 14:28:12 +0300

    Redesigned coin chart tab *added i18n

--- src/com/cryptocurrency/UI/coin/chart/CoinPriceStatistics.tsx
+++ src/com/cryptocurrency/UI/coin/chart/CoinPriceStatistics.tsx
@@ -1,168 +1,206 @@
-import { Box, Typography, Grid, Divider } from '@mui/material'
-import { CryptoState } from '../../../context/CryptoContext';
-import { numberWithCommas } from '../../table/CoinsTable';
+import {Box, CircularProgress, Divider, Grid, Typography} from '@mui/material'
+import {CryptoState} from '../../../context/CryptoContext';
+import {numberWithCommas} from '../../table/CoinsTable';
+import CoinProps from "../../../model/coin";
+import {useEffect, useState} from "react";
+import CoinService from "../../../service/CoinService";
+import ResponseProps from "../../../model/response";
+import ErrorProps from "../../../model/error";
+import {useIntl} from "react-intl";
 
-const CoinPriceStatistics = ({ coin }: any) => {
-  const { currency, symbol } = CryptoState();
+interface ICoinPriceStatisticsProps {
+  coin: CoinProps
+}
 
-  let date = new Date(coin?.last_updated)
+const CoinPriceStatistics = ({coin}: ICoinPriceStatisticsProps) => {
+  const intl = useIntl()
+  const {currency, symbol} = CryptoState();
+  const [coinGeskoMarketData, setCoinGeskoMarketData] = useState<any>()
+
+  useEffect(() => {
+    CoinService.fetchSingleCoinFromGesko(coin.id)
+      .then((resp: ResponseProps) => {
+        setCoinGeskoMarketData(resp.data.market_data)
+      })
+      .catch((err: ErrorProps) => {
+        console.log(err)
+      });
+  }, [currency]);
+
+  let date = new Date(coin.coinMarket[0].lastUpdated)
   return (
     <>
-      <Box
-        sx={{
-          boxShadow: 0,
-          width: '100%',
-          height: '100%',
-          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#808080 ' : '#C0C0C0'),
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          p: 1,
-          m: 1,
-          borderRadius: 2,
-          textAlign: 'center',
-          fontSize: '1rem',
-          fontWeight: '700',
-        }}
-      >
-        <Typography variant="h5">
-          <Box sx={{ fontWeight: 'bold' }}>{coin?.symbol.toUpperCase() + " Статистика Цены"}</Box>
-        </Typography>
-        <Grid container spacing={2} sx={{ marginTop: 2 }}>
-          <Grid item xs={12}>
-            <Grid container spacing={2} >
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-start" }}>
-                  {"Цена " + coin?.name}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  {symbol + " "}
-                  {
-                    numberWithCommas(
-                      coin?.market_data.current_price[currency.toLowerCase()]
-                    )
-                  }
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider />
-          </Grid>
+      {
+        coinGeskoMarketData !== undefined
+          ? (
+            <Box
+              sx={{
+                boxShadow: 0,
+                width: '100%',
+                height: '100%',
+                bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#808080 ' : '#C0C0C0'),
+                color: (theme) =>
+                  theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+                p: 1,
+                m: 1,
+                borderRadius: 2,
+                textAlign: 'center',
+                fontSize: '1rem',
+                fontWeight: '700',
+              }}
+            >
+              <Typography variant="h5">
+                <Box sx={{fontWeight: 'bold'}}>
+                  {coin?.symbol.toUpperCase() + " " + intl.formatMessage({id: 'price_statistics'})}
+                </Box>
+              </Typography>
+              <Grid container spacing={2} sx={{marginTop: 2}}>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-start"}}>
+                        {intl.formatMessage({id: 'coins_table_column_2'}) + " " + coin?.name}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-end"}}>
+                        {symbol + " "}
+                        {numberWithCommas(coin.coinMarket[0].currentPrice)}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider/>
+                </Grid>
 
-          <Grid item xs={12}>
-            <Grid container spacing={2} >
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-start" }}>
-                  Последнее обновление
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  {
-                    new Intl.DateTimeFormat('en-US', {
-                      year: 'numeric', month: '2-digit', day: '2-digit',
-                      hour: '2-digit', minute: '2-digit', second: '2-digit'
-                    }).format(date)
-                  }
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider />
-          </Grid>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-start"}}>
+                        {intl.formatMessage({id: 'coin_info_last_updated'})}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-end"}}>
+                        {
+                          new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric', month: '2-digit', day: '2-digit',
+                            hour: '2-digit', minute: '2-digit', second: '2-digit'
+                          }).format(date)
+                        }
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider/>
+                </Grid>
 
-          <Grid item xs={12}>
-            <Grid container spacing={2} >
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-start" }}>
-                  {"Изменение цены за 7д"}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Box sx={{
-                    fontWeight: 'bold',
-                    fontSize: 'h6.fontSize',
-                    color: coin?.market_data.price_change_percentage_7d > 0 ? "rgb(14, 203, 129)" : "red"
-                  }}>
-                    {coin?.market_data.price_change_percentage_7d > 0 ? '+' : ''}
-                    {(Math.round(coin?.market_data.price_change_percentage_7d * 10) / 10).toString() + "%"}
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider />
-          </Grid>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-start"}}>
+                        {intl.formatMessage({id: 'coin_price_change_7d'})}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-end"}}>
+                        <Box
+                          sx={{
+                            fontWeight: 'bold',
+                            fontSize: 'h6.fontSize',
+                            color: coinGeskoMarketData.price_change_percentage_7d > 0 ? "rgb(14, 203, 129)" : "red"
+                          }}
+                        >
+                          {coinGeskoMarketData.price_change_percentage_7d > 0 ? '+' : ''}
+                          {(Math.round(coinGeskoMarketData.price_change_percentage_7d * 10) / 10).toString() + "%"}
+                        </Box>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider/>
+                </Grid>
 
-          <Grid item xs={12}>
-            <Grid container spacing={2} >
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-start" }}>
-                  {"Изменение цены за 14д"}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Box sx={{
-                    fontWeight: 'bold',
-                    fontSize: 'h6.fontSize',
-                    color: coin?.market_data.price_change_percentage_14d > 0 ? "rgb(14, 203, 129)" : "red"
-                  }}>
-                    {coin?.market_data.price_change_percentage_14d > 0 ? '+' : ''}
-                    {(Math.round(coin?.market_data.price_change_percentage_14d * 10) / 10).toString() + "%"}
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider />
-          </Grid>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-start"}}>
+                        {intl.formatMessage({id: 'coin_price_change_14d'})}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-end"}}>
+                        <Box
+                          sx={{
+                            fontWeight: 'bold',
+                            fontSize: 'h6.fontSize',
+                            color: coinGeskoMarketData.price_change_percentage_14d > 0 ? "rgb(14, 203, 129)" : "red"
+                          }}
+                        >
+                          {coinGeskoMarketData.price_change_percentage_14d > 0 ? '+' : ''}
+                          {(Math.round(coinGeskoMarketData.price_change_percentage_14d * 10) / 10).toString() + "%"}
+                        </Box>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider/>
+                </Grid>
 
-          <Grid item xs={12}>
-            <Grid container spacing={2} >
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-start" }}>
-                  {"Изменение цены за 30д"}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Box sx={{
-                    fontWeight: 'bold',
-                    fontSize: 'h6.fontSize',
-                    color: coin?.market_data.price_change_percentage_30d > 0 ? "rgb(14, 203, 129)" : "red"
-                  }}>
-                    {coin?.market_data.price_change_percentage_30d > 0 ? '+' : ''}
-                    {(Math.round(coin?.market_data.price_change_percentage_30d * 10) / 10).toString() + "%"}
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider />
-          </Grid>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-start"}}>
+                        {intl.formatMessage({id: 'coin_price_change_30d'})}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-end"}}>
+                        <Box
+                          sx={{
+                            fontWeight: 'bold',
+                            fontSize: 'h6.fontSize',
+                            color: coinGeskoMarketData.price_change_percentage_30d > 0 ? "rgb(14, 203, 129)" : "red"
+                          }}
+                        >
+                          {coinGeskoMarketData.price_change_percentage_30d > 0 ? '+' : ''}
+                          {(Math.round(coinGeskoMarketData.price_change_percentage_30d * 10) / 10).toString() + "%"}
+                        </Box>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider/>
+                </Grid>
 
-          <Grid item xs={12}>
-            <Grid container spacing={2} >
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-start" }}>
-                  {"Изменение цены за 200д"}
-                </Typography>
+                <Grid item xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-start"}}>
+                        {intl.formatMessage({id: 'coin_price_change_200d'})}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" sx={{display: "flex", justifyContent: "flex-end"}}>
+                        <Box sx={{
+                          fontWeight: 'bold',
+                          fontSize: 'h6.fontSize',
+                          color: coinGeskoMarketData.price_change_percentage_200d > 0 ? "rgb(14, 203, 129)" : "red"
+                        }}>
+                          {coinGeskoMarketData.price_change_percentage_200d > 0 ? '+' : ''}
+                          {(Math.round(coinGeskoMarketData.price_change_percentage_200d * 10) / 10).toString() + "%"}
+                        </Box>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider/>
+                </Grid>
               </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Box sx={{
-                    fontWeight: 'bold',
-                    fontSize: 'h6.fontSize',
-                    color: coin?.market_data.price_change_percentage_200d > 0 ? "rgb(14, 203, 129)" : "red"
-                  }}>
-                    {coin?.market_data.price_change_percentage_200d > 0 ? '+' : ''}
-                    {(Math.round(coin?.market_data.price_change_percentage_200d * 10) / 10).toString() + "%"}
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider />
-          </Grid>
-        </Grid>
-      </Box>
+            </Box>
+          )
+          : (
+            <CircularProgress
+              style={{color: "gold"}}
+              size={250}
+              thickness={1}
+            />
+          )
+      }
     </>
   )
 }
