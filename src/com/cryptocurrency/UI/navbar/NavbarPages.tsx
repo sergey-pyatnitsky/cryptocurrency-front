669236b68ed5e0de4a1202ae30ec27bb2a23bec9
Sergey Pyatnitsky <seruyfs@mail.ru>
2023-04-04 21:28:02 +0300

    Added missing i18n

--- src/com/cryptocurrency/UI/navbar/NavbarPages.tsx
+++ src/com/cryptocurrency/UI/navbar/NavbarPages.tsx
@@ -1,10 +1,11 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import darkLogo from '../../assets/images/icons/darkLogo.svg'
-import lightLogo from '../../assets/images/icons/lightLogo.svg'
-import { useTheme } from '@mui/material/styles';
-import { useEffect, useState } from 'react';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import darkLogo from "../../assets/images/icons/darkLogo.svg";
+import lightLogo from "../../assets/images/icons/lightLogo.svg";
+import { useTheme } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+import { useIntl } from "react-intl";
 
 interface IProps {
   role: string | null;
@@ -12,99 +13,102 @@ interface IProps {
 }
 
 const NavbarPages = (props: IProps) => {
+  const intl = useIntl();
   const theme = useTheme();
   const [navLinks, setNavLinks] = useState<React.ReactNode>();
 
-  useEffect(() => {
-    setNavLinks(getNavLinks())
-  }, [props.role, props.isAuth])
+  useEffect(() => setNavLinks(getNavLinks()), [props.role, props.isAuth]);
 
   const getNavLinks = () => {
     switch (props.role) {
-      case 'ADMIN':
+      case "ADMIN":
         return (
           <>
-            <Link to="/favorites" style={{ textDecoration: 'none' }}>
+            <Link to="/favorites" style={{ textDecoration: "none" }}>
               <Button
-                key='Favorites'
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                key="Favorites"
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Отслеживание монет
+                {intl.formatMessage({ id: "coin_tracking" })}
               </Button>
             </Link>
-            <Link to="/portfolio" style={{ textDecoration: 'none' }}>
+            <Link to="/portfolio" style={{ textDecoration: "none" }}>
               <Button
-                key='Portfolio'
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                key="Portfolio"
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Портфолио
+                {intl.formatMessage({ id: "portfolio" })}
               </Button>
             </Link>
 
-            <Link to="/users" style={{ textDecoration: 'none' }}>
+            <Link to="/users" style={{ textDecoration: "none" }}>
               <Button
-                key='Users'
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                key="Users"
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Пользователи
+                {intl.formatMessage({ id: "users" })}
               </Button>
             </Link>
           </>
-        )
-      case 'USER':
+        );
+      case "USER":
         return (
           <>
-            <Link to="/favorites" style={{ textDecoration: 'none' }}>
+            <Link to="/favorites" style={{ textDecoration: "none" }}>
               <Button
-                key='Favorites'
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                key="Favorites"
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Отслеживание монет
+                {intl.formatMessage({ id: "coin_tracking" })}
               </Button>
             </Link>
 
-            <Link to="/portfolio" style={{ textDecoration: 'none' }}>
+            <Link to="/portfolio" style={{ textDecoration: "none" }}>
               <Button
-                key='Portfolio'
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                key="Portfolio"
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Портфолио
+                {intl.formatMessage({ id: "portfolio" })}
               </Button>
             </Link>
           </>
-        )
+        );
       default:
         return "";
     }
-  }
+  };
 
   return (
     <>
       <Link to="/">
-        <img src={theme.palette.mode === 'dark' ? darkLogo : lightLogo} alt="" />
+        <img
+          src={theme.palette.mode === "dark" ? darkLogo : lightLogo}
+          alt=""
+        />
       </Link>
 
-      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+      <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
         {navLinks}
       </Box>
 
-      {props.isAuth
-        ? <></>
-        : <>
-          <Link to="/login" style={{ textDecoration: 'none' }}>
+      {props.isAuth ? (
+        <></>
+      ) : (
+        <>
+          <Link to="/login" style={{ textDecoration: "none" }}>
             <Button
               variant="contained"
               color="primary"
-              key='login'
-              sx={{ my: 2, color: 'white', display: 'block' }}
+              key="login"
+              sx={{ my: 2, color: "white", display: "block" }}
             >
               Войти
             </Button>
           </Link>
         </>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
 export default NavbarPages;
