fb2cf6cadf5deecbddd8771e23c8ee6587625ea2
Sergey Pyatnitsky <seruyfs@mail.ru>
2023-04-04 21:44:14 +0300

    Edited CoinsTable.tsx *added MUI DataGrid

--- src/com/cryptocurrency/UI/table/CoinsTable.tsx
+++ src/com/cryptocurrency/UI/table/CoinsTable.tsx
@@ -1,14 +1,19 @@
-import {useEffect, useState} from "react";
-import {Container, LinearProgress, TextField, Typography,} from "@mui/material";
-import {CryptoState} from "../../context/CryptoContext";
-import {useIntl} from 'react-intl';
-import {useNavigate} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import {
+  Container,
+  LinearProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { CryptoState } from "../../context/CryptoContext";
+import { useIntl } from "react-intl";
+import { useNavigate } from "react-router-dom";
 import CoinProps from "../../model/coin";
 import CoinService from "../../service/CoinService";
 import ResponseProps from "../../model/response";
 import ErrorProps from "../../model/error";
 
-import {DataGrid, GridColDef, GridRowsProp, ruRU} from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridRowsProp, ruRU } from "@mui/x-data-grid";
 
 export function numberWithCommas(x: number) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -16,14 +21,14 @@ export function numberWithCommas(x: number) {
 
 export default function CoinsTable() {
   const navigate = useNavigate();
-  const intl = useIntl()
+  const intl = useIntl();
 
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [pageSize, setPageSize] = useState<number>(5);
 
-  const {currency, symbol} = CryptoState();
+  const { currency, symbol } = CryptoState();
 
   const handleSearch = () => {
     return coins.filter(
@@ -35,45 +40,67 @@ export default function CoinsTable() {
 
   const rows: GridRowsProp = handleSearch().map((coin: CoinProps) => {
     return {
-      id: coin.id, col1: coin.name, col2: coin.coinMarket[0].currentPrice,
-      col3: coin.coinMarket[0].priceChangePercentage24h, col4: coin.coinMarket[0].marketCap, col5: coin
+      id: coin.id,
+      col1: coin.name,
+      col2: coin.coinMarket[0].currentPrice,
+      col3: coin.coinMarket[0].priceChangePercentage24h,
+      col4: coin.coinMarket[0].marketCap,
+      col5: coin,
     };
   });
 
   const columns: GridColDef[] = [
     {
-      field: 'col1',
+      field: "col1",
       flex: 1,
       minWidth: 100,
       maxWidth: 500,
-      headerName: intl.formatMessage({id: 'coins_table_column_1'}),
+      headerName: intl.formatMessage({ id: "coins_table_column_1" }),
       renderCell: (params: any) => {
         return (
           <>
-            <img src={params.row.col5.image} alt={params.row.col5.name} height="50" style={{marginBottom: 10}}/>
-            <div style={{display: "flex", flexDirection: "column"}}>
-              <span style={{textTransform: "uppercase", fontSize: 22,}}>{params.row.col5.symbol}</span>
-              <span style={{color: "darkgrey"}}>{params.row.col5.name}</span>
+            <img
+              src={params.row.col5.image}
+              alt={params.row.col5.name}
+              height="50"
+              style={{ marginBottom: 10 }}
+            />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ textTransform: "uppercase", fontSize: 22 }}>
+                {params.row.col5.symbol}
+              </span>
+              <span style={{ color: "darkgrey" }}>{params.row.col5.name}</span>
             </div>
           </>
-        )
-      }
+        );
+      },
     },
     {
-      field: 'col2', flex: 1, minWidth: 50, maxWidth: 150, headerName: intl.formatMessage({id: 'coins_table_column_2'}),
+      field: "col2",
+      flex: 1,
+      minWidth: 50,
+      maxWidth: 150,
+      headerName: intl.formatMessage({ id: "coins_table_column_2" }),
       renderCell: (params: any) => {
         return (
           <>
             {symbol}{" "}
-            {numberWithCommas(Number(params.row.col5.coinMarket[0].currentPrice.toFixed(2)))}
+            {numberWithCommas(
+              Number(params.row.col5.coinMarket[0].currentPrice.toFixed(2))
+            )}
           </>
-        )
-      }
+        );
+      },
     },
     {
-      field: 'col3', flex: 1, minWidth: 50, maxWidth: 150, headerName: intl.formatMessage({id: 'coins_table_column_3'}),
+      field: "col3",
+      flex: 1,
+      minWidth: 50,
+      maxWidth: 150,
+      headerName: intl.formatMessage({ id: "coins_table_column_3" }),
       renderCell: (params: any) => {
-        const profit: any = params.row.col5.coinMarket[0].priceChangePercentage24h > 0;
+        const profit: any =
+          params.row.col5.coinMarket[0].priceChangePercentage24h > 0;
         return (
           <div
             style={{
@@ -84,95 +111,105 @@ export default function CoinsTable() {
             {profit && "+"}
             {params.row.col5.coinMarket[0].priceChangePercentage24h.toFixed(2)}%
           </div>
-        )
-      }
+        );
+      },
     },
     {
-      field: 'col4',
+      field: "col4",
       flex: 1,
       minWidth: 100,
       maxWidth: 200,
-      headerName: intl.formatMessage({id: 'coins_table_column_4'}),
+      headerName: intl.formatMessage({ id: "coins_table_column_4" }),
       renderCell: (params: any) => {
         return (
           <>
             {symbol}{" "}
             {numberWithCommas(
-              Number(params.row.col5.coinMarket[0].marketCap.toString().slice(0, -6))
-            )}M
+              Number(
+                params.row.col5.coinMarket[0].marketCap.toString().slice(0, -6)
+              )
+            )}
+            M
           </>
-        )
-      }
+        );
+      },
     },
-    {field: 'col5', flex: 0, maxWidth: 0, hide: true, hideable: false, pinnable: false},
+    {
+      field: "col5",
+      flex: 0,
+      maxWidth: 0,
+      hide: true,
+      hideable: false,
+      pinnable: false,
+    },
   ];
 
-  let locale
-  (localStorage.getItem('locale') == null || localStorage.getItem('locale') == 'ru-RU')
-    ? locale = ruRU.components.MuiDataGrid.defaultProps.localeText
-    : locale = undefined
+  let locale;
+  localStorage.getItem("locale") == null ||
+  localStorage.getItem("locale") === "ru-RU"
+    ? (locale = ruRU.components.MuiDataGrid.defaultProps.localeText)
+    : (locale = undefined);
 
   useEffect(() => {
     setLoading(true);
     CoinService.fetchCoinList(currency, search)
       .then((resp: ResponseProps) => {
-        setCoins(resp.data)
+        setCoins(resp.data);
         setLoading(false);
       })
       .catch((err: ErrorProps) => {
-        console.log(err)
-        setLoading(false)
+        console.log(err);
+        setLoading(false);
       });
   }, [currency]);
 
   return (
-    <Container style={{textAlign: "center", marginBottom: 40}}>
-      <Typography
-        variant="h4"
-        style={{margin: 18, fontFamily: "Montserrat"}}
-      >
-        {intl.formatMessage({id: 'coins_table_title'})}
+    <Container style={{ textAlign: "center", marginBottom: 40 }}>
+      <Typography variant="h4" style={{ margin: 18, fontFamily: "Montserrat" }}>
+        {intl.formatMessage({ id: "coins_table_title" })}
       </Typography>
       <TextField
-        label={intl.formatMessage({id: 'coins_table_search_text'})}
+        label={intl.formatMessage({ id: "coins_table_search_text" })}
         variant="outlined"
-        style={{marginBottom: 20, width: "100%"}}
+        style={{ marginBottom: 20, width: "100%" }}
         onChange={(e) => setSearch(e.target.value.toLowerCase())}
       />
 
-      {loading
-        ? (
-          <LinearProgress style={{backgroundColor: "gold"}}/>
-        )
-        : (
-          <div>
-            <DataGrid autoHeight rows={rows} columns={columns} localeText={locale}
-                      pageSize={pageSize}
-                      onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                      rowsPerPageOptions={[5, 10, 20]}
-                      pagination
-                      rowHeight={100}
-                      onSelectionModelChange={row => navigate(`/coins/${row}`)}
-                      sx={{
-                        boxShadow: 2,
-                        border: 0,
-                        backgroundColor: 'background.paper',
-                        "& .MuiDataGrid-columnHeaders": {
-                          fontWeight: "bold",
-                          fontSize: 16,
-                          fontFamily: "Montserrat",
-                          color: "text.primary"
-                        },
-                        '& .MuiDataGrid-cell': {
-                          height: '100',
-                        },
-                        '& .MuiDataGrid-row': {
-                          height: '200',
-                        },
-                      }}/>
-          </div>
-        )
-      }
+      {loading ? (
+        <LinearProgress style={{ backgroundColor: "gold" }} />
+      ) : (
+        <div>
+          <DataGrid
+            autoHeight
+            rows={rows}
+            columns={columns}
+            localeText={locale}
+            pageSize={pageSize}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            rowsPerPageOptions={[5, 10, 20]}
+            pagination
+            rowHeight={100}
+            onSelectionModelChange={(row) => navigate(`/coins/${row}`)}
+            sx={{
+              boxShadow: 2,
+              border: 0,
+              backgroundColor: "background.paper",
+              "& .MuiDataGrid-columnHeaders": {
+                fontWeight: "bold",
+                fontSize: 16,
+                fontFamily: "Montserrat",
+                color: "text.primary",
+              },
+              "& .MuiDataGrid-cell": {
+                height: "100",
+              },
+              "& .MuiDataGrid-row": {
+                height: "200",
+              },
+            }}
+          />
+        </div>
+      )}
     </Container>
   );
 }
